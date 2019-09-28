033909d2e764a4013c713d7fe517d633d7b88183
richieyang <zyflash@163.com>
2019-09-28 21:23:43 +0800

    start to learn bash

---
+++ redirect.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+date > test1
+
+who >> test1
+
+wc < test1
+
+
---
+++ variable.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set
+echo UID: $UID
+
+echo "The cost of the item is \$15"
+
+var1=10
+var2="still more testing"
+guest="Katie"
+
+echo "$guest checked in $var1 days ago"
+
+testing=`date`
+testing1=$(date)
+
+echo "today is $testing1"
+today=$(date +%y%m%d)
+
+echo "today is $today"
+
+
